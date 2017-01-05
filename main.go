ec29d3cd5b3a1d35b10f658aada2a4924c204eac
Quy Vu <tranquy2512@gmail.com>
2017-01-05 18:12:25 +0000

    Update port env

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"./modules/user"
 	"net/http"
+	"os"
 )
 
 func main() {
 	user.Gonow()
-	http.ListenAndServe(":8080", nil)
+	var port string
+	if os.Getenv("PORT") != "" {
+		port = ":" + os.Getenv("PORT")
+	} else {
+		port = ":8080"
+	}
+	http.ListenAndServe(port, nil)
 }
